06d926e0b5ad069cea0151ce84649d2e4100640d
Lauri Hiltunen <hiltusenlauri@gmail.com>
2018-05-04 20:36:00 +0300

    Add command and query controllers for CQRS

---
+++ src/main/java/com/github/laurihi/ftc/challengeservice/controller/command/CommandController.java
@@ -0,0 +1,12 @@
+package com.github.laurihi.ftc.challengeservice.controller.command;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@EnableSwagger2
+@RestController
+@RequestMapping("/cmd")
+public class CommandController {
+
+}
