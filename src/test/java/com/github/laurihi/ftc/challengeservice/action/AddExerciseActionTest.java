8c327d2dcf751d415d2dd875eba5ed487212bbdf
Lauri Hiltunen <hiltusenlauri@gmail.com>
2018-05-06 19:11:01 +0300

    AddExerciseAction tests

---
+++ src/test/java/com/github/laurihi/ftc/challengeservice/action/AddExerciseActionTest.java
@@ -0,0 +1,28 @@
+package com.github.laurihi.ftc.challengeservice.action;
+
+import com.github.laurihi.ftc.challengeservice.model.ExerciseEventDescriptor;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Profile;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.junit.Assert.*;
+
+@Profile("test")
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
+public class AddExerciseActionTest {
+
+    @Autowired
+    AddExerciseAction addExerciseAction;
+
+    @Test
+    public void shouldHaveCorrectMethodSignature() {
+        ExerciseEventDescriptor descriptor = null;
+        addExerciseAction.addExercise(descriptor);
+    }
+}
