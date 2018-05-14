a895b23b36a2dc3edefaa5e0a8a11cf7bc65dc84
Lauri Hiltunen <hiltusenlauri@gmail.com>
2018-05-14 21:49:50 +0300

    Remove points per unit from descriptor, data should come from challenge's exercise instead.

--- src/main/java/com/github/laurihi/ftc/challengeservice/model/ExerciseEventDescriptor.java
+++ src/main/java/com/github/laurihi/ftc/challengeservice/model/ExerciseEventDescriptor.java
@@ -3,7 +3,6 @@ package com.github.laurihi.ftc.challengeservice.model;
 public class ExerciseEventDescriptor {
 
     private String exerciseName;
-    private int pointsPerUnit;
     private double amount;
 
     public String getExerciseName() {
@@ -12,14 +11,6 @@ public class ExerciseEventDescriptor {
 
     public void setExerciseName(String exerciseName) {
         this.exerciseName = exerciseName;
-    }
-
-    public int getPointsPerUnit() {
-        return pointsPerUnit;
-    }
-
-    public void setPointsPerUnit(int pointsPerUnit) {
-        this.pointsPerUnit = pointsPerUnit;
     }
 
     public double getAmount() {
